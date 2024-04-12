a0262889eb6fbb5024ae15e61b5b6db363108a60
Raphaël de Gail <raphael.de.gail@gmail.com>
2024-04-12 18:28:26 +0200

    Fix name variable syntax

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ locals {
   // Default IP address range for the worksapce network
   base_cidr_block = "10.1.0.0/27"
   wrk_name        = lower(replace(data.google_folder.workspace_folder.display_name, " Workspace", ""))
-  name            = join("-", [terraform.workspace, local.wrk_name])
+  name            = lower(join("-", [terraform.workspace, local.wrk_name]))
 }
 
 resource "random_string" "random" {
